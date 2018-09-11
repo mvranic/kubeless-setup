f97e3ab095a3ebcf1c7b0505021ce44ed29c0e2e
Marko Vranic <Marko.Vranic@simcorp.com>
2018-09-11 16:27:23 +0200

    Removing 2 dots.

--- setup.sh
+++ setup.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 # file: setup.sh
 
+# Default settings:
 sudo cp ./kubeless.sh  /etc/profile.d 
 source /etc/profile.d/kubeless.sh 
 
+# Install make:
+sudo apt install make
+
+# Run build:
 export KUBELESS_WORKING_DIR=$GOROOT/src/github.com/kubeless
 
 sudo mkdir -p $KUBELESS_WORKING_DIR
 cd $KUBELESS_WORKING_DIR
-git clone https://github.com/mvranic/kubeless
+sudo git clone https://github.com/mvranic/kubeless
 cd $KUBELESS_WORKING_DIR/kubeless
-git remote add upstream https://github.com/kubeless/kubeless.git
+sudo git remote add upstream https://github.com/kubeless/kubeless.git
 
 # Never push to upstream master
 git remote set-url --push upstream no_push
